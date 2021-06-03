9ead1f6083033704718c26f3763a5ae7f3ea08df
arodde <roddealain@gmail.comgit config --global user.email roddealain@gmail.comgit config --global user.email roddealain@gmail.com>
2021-06-03 09:05:42 +0200

    ajour row in center via module flutter outline

--- lib/main.dart
+++ lib/main.dart
@@ -18,25 +18,21 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Expanded(
-          flex: 1,
-          child: Image(
-            width: 160,
-            image: AssetImage('images/dice1.png'),
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: Image(
+              image: AssetImage('images/dice1.png'),
+            ),
           ),
-        ),
-        Expanded(
-          flex: 1,
-          child: Image(
-            width: 160,
-            image: AssetImage('images/dice2.png'),
+          Expanded(
+            child: Image(
+              image: AssetImage('images/dice2.png'),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
